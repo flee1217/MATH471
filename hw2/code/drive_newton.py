c1ef92401328d2a5b270106dbc88a792a1183cfe
Frederick Lee <flee1217@ct-lobol-h.colleges.ad.unm.edu>
2018-09-15 16:55:33 -0600

    removed compound plots + added more comments

--- hw2/code/drive_newton.py
+++ hw2/code/drive_newton.py
@@ -29,19 +29,31 @@ def f3(x, d):
 fcn_list = [f1,f2,f3]
 
 # tunable parameters
-x0 = .5
+x0 = 0.5
 maxIterations = 30
 tolerance = 10 ** (-10)
 
 # fcn plot parameters
-plot_scale_factor = 0.1
+plot_lim_scale_factor = 1.1
 plot_res     = 1000
 
+init_root_label_x = [ 0.65, 0.65, 0.39 ]
+init_root_label_y = [ 0.8, 0.85, 0.6  ]
+final_root_label_x = [ 0.4, 0.5, 0.35 ]
+final_root_label_y = [ 0.6, 0.4, 0.44 ]
+plot_titles = ['$f(x) = x^2$', '$f(x) = x$', '$f(x) = \sin(x) + \cos(x^2)$']
+
 for i in range(len(fcn_list)):
+    # for this lab, we want to use a initial guess of -0.5 for the third
+    # function, mostly to showcase the fast convergence using newton's
+    # method
+    if (i == 2):
+        x0 = -x0
     data = newton(fcn_list[i], x0, maxIterations, tolerance )
     print(data)
     print('number of iterations: %d' %len(data))
-
+    
+    '''
     # this compound plot holds two vertically arranged subplots
     f, plots = plt.subplots(2, sharex = True)
 
@@ -60,9 +72,9 @@ for i in range(len(fcn_list)):
     f.savefig('newton_fig%d.png' %(i+1), dpi=None, format='png', bbox_inches='tight',pad_inches=0.1,)
     # dumping root finding data into .tex file
     savetxt('newton_data%s.tex' %(i+1),data,fmt='%.5e',delimiter='  &  ',newline=' \\\\\n')
-
-    # now, we plot the function
-
+    '''
+    
+    # now, we plot the function on a linear x-y graph
     xs = data[:,0].tolist()
     xs.insert(0, x0)
     ys = data[:,4].tolist()
@@ -70,7 +82,7 @@ for i in range(len(fcn_list)):
     
     # we would like to plot only parts of our function that are necessary
     # for visualizing the iterated root approximations, so we set the xlimits
-    # and ylimits for our plots to 
+    # and ylimits for our plots to values close to the extremum x or y values
     y_max = max(ys)
     y_min = min(ys)
     x_max = max(xs)
@@ -87,19 +99,55 @@ for i in range(len(fcn_list)):
     x_center = x_min + x_d/2
     y_center = y_min + y_d/2
 
-    x_ = linspace(x_min-max_diff*plot_scale_factor, x_max+max_diff*plot_scale_factor,plot_res + 1)
-    y_ = fcn_list[i](x_,0)
+    x_lower_bound = x_min - max_diff * (plot_lim_scale_factor - 1)
+    x_upper_bound = x_max + max_diff * (plot_lim_scale_factor - 1)
+    
     g, gplot = plt.subplots()
-    gplot.plot(x_,y_)
+    
+
+
+
+
     
     # plotting the x and f(x) iterates
-    gplot.scatter(x0,fcn_list[i](x0,0),c='red')
-    gplot.scatter(data[:,0],data[:,4],c='red')
+    gplot.plot(xs[:], ys[:], 'bo--', c='red')
+    
+    # setting the dynamically determined plot extrema
+    x_lower_lim = x_center - (max_diff / 2) * (plot_lim_scale_factor)
+    x_upper_lim = x_center + (max_diff / 2) * (plot_lim_scale_factor)
+    y_lower_lim = y_center - (max_diff / 2) * (plot_lim_scale_factor)
+    y_upper_lim = y_center + (max_diff / 2) * (plot_lim_scale_factor)
 
-    gplot.set_xlim(x_center - (max_diff / 2) * (1.1), x_center + (max_diff / 2) * (1.1))
-    gplot.set_ylim(y_center - (max_diff / 2) * (1.1), y_center + (max_diff / 2) * (1.1))
+    lim_max = max(abs(x_lower_lim),abs(x_upper_lim),abs(y_lower_lim),abs(y_upper_lim))
+    # gplot.set_xlim(x_lower_lim, x_upper_lim)
+    # gplot.set_ylim(y_lower_lim, y_upper_lim)
+    
+    gplot.set_xlim(-lim_max, lim_max)
+    gplot.set_ylim(-lim_max, lim_max)
 
+    # we would like to superimpose the root approximations over a graph of the function in question
+    # x_ = linspace(x_lower_bound, x_upper_bound,plot_res + 1)
+    x_ = linspace(-lim_max, lim_max, plot_res + 1)
+    
+    y_ = fcn_list[i](x_,0)
+    gplot.plot(x_,y_,c='black')
+
+    gplot.axvline(linewidth = 1, color = 'k')
+    gplot.axhline(linewidth = 1, color = 'k')
+    
+    gplot.set_xlabel('$x$')
+    gplot.set_ylabel('$f(x)$')
+    gplot.set_title('%s' %plot_titles[i])
     gplot.grid(True, linestyle='--')
+    gplot.set_aspect(aspect='equal')
+    
+    # these two functions create and place the labels for the initial and final root approximations
+    gplot.annotate('$(x_0,f(x_0))$', xy=(xs[0],ys[0]),xytext=(init_root_label_x[i],init_root_label_y[i]),
+                   textcoords='figure fraction',arrowprops=dict(arrowstyle="->",connectionstyle="arc3"),size=14)
+
+    gplot.annotate('$(x_n,f(x_n))$', xy=(xs[-1],ys[-1]),xytext=(final_root_label_x[i],final_root_label_y[i]),
+                   textcoords='figure fraction',arrowprops=dict(arrowstyle="->",connectionstyle="arc3"),size=14)
+
     g.savefig('newton_plot%d.png' %(i+1), dpi=None, format='png', bbox_inches='tight',pad_inches=0.1,)
     plt.show()
 
