bb258075a47fb76eec26a620f4df4edcbda9688c
Frederick Lee <flee1217@ct-dsh144-r.unm.edu>
2018-09-15 14:45:04 -0600

    added func plots with formatting

--- hw2/code/drive_newton.py
+++ hw2/code/drive_newton.py
@@ -33,6 +33,10 @@ x0 = .5
 maxIterations = 30
 tolerance = 10 ** (-10)
 
+# fcn plot parameters
+plot_scale_factor = 0.1
+plot_res     = 1000
+
 for i in range(len(fcn_list)):
     data = newton(fcn_list[i], x0, maxIterations, tolerance )
     print(data)
@@ -56,9 +60,42 @@ for i in range(len(fcn_list)):
     f.savefig('newton_fig%d.png' %(i+1), dpi=None, format='png', bbox_inches='tight',pad_inches=0.1,)
     # dumping root finding data into .tex file
     savetxt('newton_data%s.tex' %(i+1),data,fmt='%.5e',delimiter='  &  ',newline=' \\\\\n')
-    # Here, you can post-process data, and test convergence rates 
 
-    # Also, use your skills from the last lab, and use savetxt() to dump 
-    # data and/or convergence rates to a text file
+    # now, we plot the function
 
+    xs = data[:,0].tolist()
+    xs.insert(0, x0)
+    ys = data[:,4].tolist()
+    ys.insert(0, fcn_list[i](x0,0))
+    
+    # we would like to plot only parts of our function that are necessary
+    # for visualizing the iterated root approximations, so we set the xlimits
+    # and ylimits for our plots to 
+    y_max = max(ys)
+    y_min = min(ys)
+    x_max = max(xs)
+    x_min = min(xs)
+
+    y_d = y_max - y_min
+    x_d = x_max - x_min
+    max_diff = max(y_d, x_d)
+
+    x_center = x_min + x_d/2
+    y_center = y_min + y_d/2
+
+    x_ = linspace(x_min-max_diff*plot_scale_factor, x_max+max_diff*plot_scale_factor,plot_res + 1)
+    y_ = fcn_list[i](x_,0)
+    g, gplot = plt.subplots()
+    gplot.plot(x_,y_)
+    
+    # plotting the x and f(x) iterates
+    gplot.scatter(x0,fcn_list[i](x0,0),c='red')
+    gplot.scatter(data[:,0],data[:,4],c='red')
+
+    gplot.set_xlim(x_center - (max_diff / 2) * (1.1), x_center + (max_diff / 2) * (1.1))
+    gplot.set_ylim(y_center - (max_diff / 2) * (1.1), y_center + (max_diff / 2) * (1.1))
+
+    gplot.grid(True, linestyle='--')
+    g.savefig('newton_plot%d.png' %(i+1), dpi=None, format='png', bbox_inches='tight',pad_inches=0.1,)
+    plt.show()
 
