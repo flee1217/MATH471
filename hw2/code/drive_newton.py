910ecfabf4706e33566306b78de72f8ba731aa34
Frederick Lee <flee1217@ct-lobol-h.colleges.ad.unm.edu>
2018-09-16 14:14:08 -0600

    adding labels to f(x)/alpha plots

--- hw2/code/drive_newton.py
+++ hw2/code/drive_newton.py
@@ -26,11 +26,10 @@ def f3(x, d):
     elif d == 1:
         return cos(x) - 2*x*sin(x**2)
 
-
 fcn_list = [f1,f2,f3]
 
 # tunable parameters
-x0 = 0.5
+x0s = [ 0.5, 0.5, -.5 ]
 maxIterations = 30
 tolerance = 10 ** (-10)
 
@@ -48,43 +47,48 @@ for i in range(len(fcn_list)):
     # for this lab, we want to use a initial guess of -0.5 for the third
     # function, mostly to showcase the fast convergence using newton's
     # method
-    if (i == 2):
-        x0 = -x0
-    data = newton(fcn_list[i], x0, maxIterations, tolerance )
+    data = newton(fcn_list[i], x0s[i], maxIterations, tolerance )
     print(data)
     print('number of iterations: %d' %len(data))
     
-    '''
+
     # this compound plot holds two vertically arranged subplots
     f, plots = plt.subplots(2, sharex = True)
+    
 
-    # the first subplot graphs the function value as a function
+    # the first subplot graphs the function value
     # of the current root approximation as the root-finder
     # iterates
     plots[0].plot(range(len(data)), data[:,4])
-    # the second subplot graphs the alpha value which is the
-    # result of the expression ( log (A) / log (B) ), where
-    # A = | X_(n+1) - X_n     | / | X_n     - X_(n-1) |
-    # B = | X_n     - X_(n-1) | / | X_(n-1) - X_(n-2) |
+    # the second subplot graphs the alpha values as the iterations
+    # increase
     plots[1].plot(range(len(data)),data[:,5])
+
+    # formatting
+    plots[0].set_ylabel('$ f(x_n) $',fontsize=16)
+    plots[1].set_ylabel(r'$ \alpha $',fontsize=16)
+    plots[1].set_xlabel('$iteration$')
+    
+    plt.xticks(np.arange(0,len(data),1))
+
     # showing the compound plot
     plt.show()
     # saving the compound plot
     f.savefig('newton_fig%d.png' %(i+1), dpi=None, format='png', bbox_inches='tight',pad_inches=0.1,)
-    '''
+
     # dumping root finding data into .tex file
     savetxt('newton_data%s.tex' %(i+1),data,fmt='%.5e',delimiter='  &  ',newline=' \\\\\n')
     
-    
+    ############################################################################
     # now, we plot the function on a linear x-y graph
     g, gplot = plt.subplots()
 
     # data does not contain the initial root guess, so we insert both x0 and f(x0) at the
     # front of each root value and 
     xs = data[:,0].tolist()
-    xs.insert(0, x0)
+    xs.insert(0, x0s[i])
     ys = data[:,4].tolist()
-    ys.insert(0, fcn_list[i](x0,0))
+    ys.insert(0, fcn_list[i](x0s[i],0))
     
     # we would like to plot only parts of our function that are necessary
     # for visualizing the iterated root approximations, so we set the xlimits
