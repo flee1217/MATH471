3720b0c982c14e8b22a7df501f388b92b3790b14
Frederick Lee <flee1217@ct-lobol-q.colleges.ad.unm.edu>
2018-10-26 16:57:24 -0600

    add thread error plot formatting

--- hw4/code/plot_error.py
+++ hw4/code/plot_error.py
@@ -7,13 +7,23 @@ ns = np.arange(1,6)
 f, fplot = plt.subplots()
 
 for i,e in enumerate(error):
-    fplot.loglog(ns, e, label= 'threads: ' + str(i+1) + '', linewidth = 2)
+    fplot.loglog(ns, e, label= '$Threads:$ $' + str(i+1) + '$', linewidth = 2)
 
 # reference plots
-#fplot.loglog(ns, ns**(-1.)/10.**8.)
-#fplot.loglog(ns, ns**(-2.)/10.**8.)
+fplot.loglog(ns, ns**(-1.)/10.**8,
+             label = '$O(n^{-1})$')
+fplot.loglog(ns, ns**(-2.)/10.**8,
+             label = '$O(n^{-2})$')
 
 # nice formatting
+plt.title('$Error$ $vs$ $N$',
+          fontsize = 16)
+fplot.set_xlabel('$N$',
+                 fontsize = 16)
+fplot.set_ylabel('$Error$',
+                 fontsize = 16,
+                 rotation = 0,
+                 rotation_mode = 'anchor')
 fplot.legend()
 
-plt.show()
+plt.savefig('error_thread.png',dpi=600,bbox_inches = None, pad_inches = 0.1)
